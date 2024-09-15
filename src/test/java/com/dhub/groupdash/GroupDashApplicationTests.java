0f1d9d34f730ba3252efbf0e6d676793fc29e2f2
Tianyou-Luo <tianyoul@bu.edu>
2024-09-15 16:51:48 -0400

    commiting the initialized GroupDash Project

---
+++ src/test/java/com/dhub/groupdash/GroupDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhub.groupdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupDashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
